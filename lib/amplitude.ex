2eec12b8b1800324945f51bbe3f15f5da2d27ba7
Peter B <peter.bonney@gmail.com>
2016-09-08 22:15:45 -0400

    basic tracking and a simple test

--- lib/amplitude.ex
+++ lib/amplitude.ex
@@ -1,2 +1,24 @@
 defmodule Amplitude do
+  use Amplitude.API
+
+  @docmodule """
+  Tools for getting data from the Worldping API
+
+  Each Worldping API query accesses a different URL, not just different query
+  arguments, so for clarity each type of Worldping API call gets its own
+  function
+  """
+
+  @doc """
+  Track an event with specified properties
+
+      iex> {:ok, response} = Amplitude.track("my_event", "jdoe_123", %{"ip" => "127.0.0.1"}, %{"cohort" => "Test A"})
+      ...> response
+      "success"
+  """
+  def track(event_type, user_id, event_props \\ %{}, user_props \\ %{})
+  def track(event_type, user_id, event_props, user_props) do
+    %{"event_type" => event_type, "user_id" => user_id, "event_properties" => event_props, "user_properties" => user_props}
+    |> api_track
+  end
 end
---
+++ lib/api/real.ex
@@ -0,0 +1,24 @@
+defmodule Amplitude.API.Real do
+  use HTTPoison.Base
+
+  defp api_host, do: Application.get_env(:amplitude, :api_host)
+  defp api_key, do: Application.get_env(:amplitude, :api_key)
+  defp json_header, do: ["Content-Type": "application/json"]
+
+  def api_track(params) do
+    case get(api_host, [], params: [api_key: api_key, event: Poison.encode!(params)]) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body}
+      {:ok, %HTTPoison.Response{status_code: 404, body: body}} ->
+        {:error, body}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
+  end
+
+  # validate Poison response and strip out json value
+  def verify_json({:ok, json}), do: json
+  def verify_json({_, response}), do: "#{inspect(response)}"
+
+  defp process_request_headers(headers), do: headers++json_header
+end
